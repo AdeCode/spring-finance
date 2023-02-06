e7e4463cae7809e7f936b9a01ef72a6b94714788
heybeecode <habeeb@springcash.co>
2023-02-06 13:53:35 +0100

    add play store link

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -9,6 +9,10 @@ import Partners from '../components/Partners'
 import Modal from '../components/Modal'
 import WaitlistForm from '../components/WaitlistForm'
 import services from '../services/FormService'
+import playWhite from '../images/play-white.png'
+import apple from '../images/apple.png'
+import appleM from '../images/appleM.png'
+
 
 
 function AboutUs() {
@@ -94,7 +98,30 @@ function AboutUs() {
                 <div className='font-medium text-[10px] tracking-tight lg:text-[39px] flex flex-col items-center lg:leading-[47px] py-[20px]  lg:py-[70px] lg:justify-center text-white'>
                     <h2 className='w-[156px] lg:w-[609px] text-center'>Start sending money to friends, family, businesses and international schools with Spring today.</h2>
                     <span className='w-[156px] lg:w-[609px] text-center lg:mt-[40px] mt-[6px]'>No stress or expensive fees.</span>
-                    <button onClick={openModal} className='text-blue-bg font-semibold text-sm lg:text-lg bg-white py-1 px-[10px] lg:py-4 lg:px-11 mt-[6px] lg:mt-10'>Join our waitlist</button>
+                    {/* <button onClick={openModal} className='text-blue-bg font-semibold text-sm lg:text-lg bg-white py-1 px-[10px] lg:py-4 lg:px-11 mt-[6px] lg:mt-10'>Join our waitlist</button> */}
+                    <div className='lg:mt-5 mt-[6px] flex lg:gap-2 gap-[2px]'>
+                        <a href='https://play.google.com/store/apps/details?id=com.getspring.finance' target='_blank' rel='noreferrer' className='flex items-center border-[2px] border-[#E2E4E8] lg:py-[6px] py-[2px] lg:px-5 px-[5px] rounded-[4px] lg:gap-4 lg:w-[189px]'>
+                            <div>
+                                <img src={playWhite} alt='play store'/>
+                            </div>
+                            <div className='flex flex-col items-start'>
+                                <h4 className='text-white font-normal lg:text-xs text-[3px]'>Get it on</h4>
+                                <h3 className='font-bold lg:text-lg text-[4.8px]'>Google Play</h3>
+                            </div>
+                        </a>
+                        <button className='flex items-center bg-[#7B61FF] lg:py-[6px] py-[2px] lg:px-5 px-1 rounded-[4px] lg:gap-4 gap-1 lg:w-[189px]'>
+                            <div className='hidden lg:flex'>
+                                <img src={apple} alt='app store'/>
+                            </div>
+                            <div className='lg:hidden'>
+                                <img src={appleM} alt='app store'/>
+                            </div>
+                            <div className='flex flex-col items-start'>
+                                <h4 className='text-white font-normal lg:text-xs text-[3px]'>Coming soon</h4>
+                                <h3 className='font-bold lg:text-lg text-[4.8px]'>App Store</h3>
+                            </div>
+                        </button>
+                    </div>
                 </div>
             </div>
             <Partners />
